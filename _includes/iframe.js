dc3d444f7c2504ba47e56496dcf5b65073c1430b
Ryan Koesters <rmkoesters@gmail.com>
2017-03-01 17:57:19 -0600

    Make iframes (e.g. YouTube videos) maintain a 16:9 aspect ratio.

---
+++ _includes/iframe.js
@@ -0,0 +1,10 @@
+// Make sure iframes maintain a 16:9 aspect ratio.
+function resize_iframes() {
+	$("iframe").each(function() {
+		var w = parseInt($(this).width());
+		var h = parseInt((w / 16) * 9);
+		$(this).height(h);
+	});
+}
+$(window).resize(resize_iframes);
+resize_iframes();
