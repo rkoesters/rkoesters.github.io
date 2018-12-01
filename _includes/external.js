2bd10b094c4691f38cb5b538a9376d0a4e0407ff
Ryan Koesters <rmkoesters@gmail.com>
2018-12-01 12:38:31 -0600

    Update external.js to avoid opener vulnerability.

--- _includes/external.js
+++ _includes/external.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$("a").filter(function(){
 		return this.hostname && this.hostname !== location.hostname;
-	}).attr("target", "_blank");
+	}).attr({
+		rel:    "noopener",
+		target: "_blank"
+	});
 });
