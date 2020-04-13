0df46e7a244f8d615c3dc73806793900bba962f9
Ryan Koesters <rmkoesters@gmail.com>
2020-04-13 02:34:53 -0700

    Rewrite iframe.js without jQuery.

--- _includes/iframe.js
+++ _includes/iframe.js
@@ -1,10 +1,8 @@
 // Make sure iframes (assumed to be videos) maintain a 16:9 aspect ratio.
 function resize_iframes() {
-	$("iframe").each(function() {
-		var w = parseInt($(this).width());
-		var h = parseInt((w / 16) * 9);
-		$(this).height(h);
-	});
+	for (let iframe of document.getElementsByTagName('iframe')) {
+		iframe.height = parseInt((iframe.offsetWidth / 16) * 9);
+	}
 }
-$(window).resize(resize_iframes);
+window.addEventListener('resize', resize_iframes);
 resize_iframes();
