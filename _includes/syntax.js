f6283c470676e38248aec671d48f233386ce0f28
Ryan Koesters <rmkoesters@gmail.com>
2020-04-13 02:57:22 -0700

    Use const instead of var in syntax.js.

--- _includes/syntax.js
+++ _includes/syntax.js
@@ -1,4 +1,4 @@
-var syntax = document.createElement('link');
+const syntax = document.createElement('link');
 syntax.href = '/css/syntax.css';
 syntax.rel = 'stylesheet';
 document.getElementsByTagName('head')[0].appendChild(syntax);
